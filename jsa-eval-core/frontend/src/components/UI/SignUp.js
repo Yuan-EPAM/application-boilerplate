2442f2b0eff8501c257269b1361216ae2733e7b2
Yuan Liao <Yuan_Liao@epam.com>
2020-01-02 19:06:27 +0800

    add basic sign up static UI

--- jsa-eval-core/frontend/src/components/UI/SignUp.js
+++ jsa-eval-core/frontend/src/components/UI/SignUp.js
@@ -1,14 +1,69 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import { TextField } from '@material-ui/core';
+import { Container, Grid, Card, Input, InputAdornment, Button } from '@material-ui/core';
+import { MailOutline, VisibilityOff } from '@material-ui/icons';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignContent: 'center',
+    height: 500
+  },
+  input: {
+    display: 'flex',
+    flexDirection: 'column',
+    margin: theme.spacing(1)
+  },
+  button: {
+    margin: 10,
+    width: '30%'
+  }
+}));
 
 const SignUp = () => {
+  const classes = useStyles();
+
   return (
-    <form>
-      <TextField label="Email Address" />
-      <TextField label="User Name" />
-      <TextField label="Password" />
-    </form>
+    <Container className={classes.root} maxWidth="sm">
+      <Card>
+        <form className={classes.input}>
+          <Grid container>
+            <Grid item xs={6}>
+              <Input id="lastName" placeholder="Last Name" />
+            </Grid>
+            <Grid item xs={6}>
+              <Input id="firstName" placeholder="First Name" />
+            </Grid>
+            <Grid item xs={12}>
+              <Input
+                id="email"
+                placeholder="Eamil Address"
+                startAdornment={
+                  <InputAdornment position="start">
+                    <MailOutline />
+                  </InputAdornment>
+                }
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <Input
+                id="password"
+                placeholder="Password"
+                startAdornment={
+                  <InputAdornment position="start">
+                    <VisibilityOff />
+                  </InputAdornment>
+                }
+              />
+            </Grid>
+          </Grid>
+          <Button className={classes.button} variant="contained" color="primary">
+            SIGN UP
+          </Button>
+        </form>
+      </Card>
+    </Container>
   );
 };
 
