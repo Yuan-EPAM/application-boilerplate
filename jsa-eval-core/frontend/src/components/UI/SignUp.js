124890d3257f22c51e3d5dc0fac8c3df4a7914ca
Yuan Liao <Yuan_Liao@epam.com>
2020-01-02 19:56:28 +0800

    wrap duplicated components into function

--- jsa-eval-core/frontend/src/components/UI/SignUp.js
+++ jsa-eval-core/frontend/src/components/UI/SignUp.js
@@ -1,19 +1,26 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import { Container, Grid, Card, Input, InputAdornment, Button } from '@material-ui/core';
-import { MailOutline, VisibilityOff } from '@material-ui/icons';
+import { AccountCircleOutlined, MailOutline, VisibilityOff } from '@material-ui/icons';
 
 const useStyles = makeStyles(theme => ({
   root: {
+    border: '3px solid yellow',
+
     display: 'flex',
-    flexDirection: 'column',
-    alignContent: 'center',
+    // flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
     height: 500
   },
-  input: {
+  inputArea: {
+    border: '2px solid skyblue',
     display: 'flex',
-    flexDirection: 'column',
-    margin: theme.spacing(1)
+    flexDirection: 'column'
+  },
+  inputItem: {
+    border: '1px solid red',
+    margin: 10
   },
   button: {
     margin: 10,
@@ -21,40 +28,52 @@ const useStyles = makeStyles(theme => ({
   }
 }));
 
+const addIcon = (position = 'start', iconComponent) => {
+  return <InputAdornment position={position}>{iconComponent}</InputAdornment>;
+};
+
+const addInputItem = (item, placeholder, iconComponent) => {
+  return;
+};
+
+const addInputItems = () => {};
+
 const SignUp = () => {
   const classes = useStyles();
 
   return (
     <Container className={classes.root} maxWidth="sm">
       <Card>
-        <form className={classes.input}>
+        <form className={classes.inputArea}>
           <Grid container>
-            <Grid item xs={6}>
-              <Input id="lastName" placeholder="Last Name" />
+            <Grid item xs={12} className={classes.inputItem}>
+              <Input
+                id="lastName"
+                placeholder="Last Name"
+                startAdornment={addIcon('start', <AccountCircleOutlined />)}
+              />
+              <Input
+                id="firstName"
+                placeholder="First Name"
+                startAdornment={addIcon('start', <AccountCircleOutlined />)}
+              />
             </Grid>
-            <Grid item xs={6}>
-              <Input id="firstName" placeholder="First Name" />
-            </Grid>
+            {/* <Grid item xs={6}>
+            </Grid> */}
             <Grid item xs={12}>
               <Input
                 id="email"
+                className={classes.inputItem}
                 placeholder="Eamil Address"
-                startAdornment={
-                  <InputAdornment position="start">
-                    <MailOutline />
-                  </InputAdornment>
-                }
+                startAdornment={addIcon('start', <MailOutline />)}
               />
             </Grid>
             <Grid item xs={12}>
               <Input
                 id="password"
+                className={classes.inputItem}
                 placeholder="Password"
-                startAdornment={
-                  <InputAdornment position="start">
-                    <VisibilityOff />
-                  </InputAdornment>
-                }
+                startAdornment={addIcon('start', <VisibilityOff />)}
               />
             </Grid>
           </Grid>
