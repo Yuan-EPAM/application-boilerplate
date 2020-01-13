f56af7b7d1429cd9d13032975fbc765e8f38b654
Yuan-EPAM <Yuan_Liao@epam.com>
2020-01-13 10:41:46 +0800

    refactor(backend): folder structure

--- jsa-eval-core/backend/test-service/src/start.js
+++ jsa-eval-core/backend/test-service/src/start.js
@@ -1,6 +1,19 @@
-require('dotenv').config();
-const app = require('./server');
+require('dotenv').config({ path: '../.env' });
+const Koa = require('koa');
+const cors = require('koa2-cors');
+const bodyParser = require('koa-bodyparser');
+const koaValidator = require('koa-async-validator');
+
+const router = require('./routers/router');
+
+const app = new Koa();
+
+app.use(cors());
+app.use(bodyParser());
+app.use(koaValidator());
+
+app.use(router.routes()).use(router.allowedMethods());
 
 app.listen(process.env.SERVER_PORT, () => {
-  console.log(`server running on ${process.env.SERVER_PORT}`);
+  console.log(`koa server running on port ${process.env.SERVER_PORT}`);
 });
